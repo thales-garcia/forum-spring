b5eb3f2945affaac9fa4a26f3cf3355c1caac4c9
Thales Garcia <thalesgsoares@gmail.com>
2021-12-02 13:55:47 -0300

    Creating TopicoDto

---
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -0,0 +1,24 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicoController {
+
+    @RequestMapping(value="/topicos")
+    public List<TopicoDto> lista(){
+        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+
+        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    }
+
+}
